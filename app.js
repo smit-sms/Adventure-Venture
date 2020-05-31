57c5b4ffb13be73f739335010944c09c7faa3ffa
smit-sms <smit16@somaiya.edu>
2020-05-31 18:08:25 +0530

    Added styling and code Refactoring

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const port = process.env.PORT || 5020;
 const routes = require('./routes/route');
 const mongoose = require('mongoose');
-const url = 'mongodb://localhost/Blogging';
+const url = 'mongodb://localhost/AdventureVenture';
 mongoose.connect(url, {useNewUrlParser:true})
 const con = mongoose.connection
 const bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({extended:true}));
+
 con.on('open', () => {
     console.log("connected...")
 })
--- routes/route.js
+++ routes/route.js
@@ -1,33 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const CampgroundsController = require("../controllers/CampgroundsController");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://pixabay.com/get/57e1dd4a4350a514f1dc84609620367d1c3ed9e04e50744075277bd4964fc3_340.jpg"},
-    {name: "Granite Hills", image: "https://pixabay.com/get/57e1dd4a4350a514f1dc84609620367d1c3ed9e04e50744075277bd4964fc3_340.jpg"},
-    {name: "Mountain Goat", image: "https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e50744075277bd4964fc3_340.jpg"}
-]
-// schema access (models)
-// const Blog = require('../models/blog')
+// landing/home page
+router.get("/", CampgroundsController.landingpage);
+// view campgrounds
+router.get("/campgrounds", CampgroundsController.campgroundspage);
+// add a new campground
+router.get("/campgrounds/new", CampgroundsController.addnewcampground);
+router.post("/campgrounds", CampgroundsController.addnew);
 
-router.get("/", (req,res)=>{
-    res.render("landing");
-})
+//
 
-router.get("/campgrounds", (req,res)=>{
-    res.render('campgrounds',{campgrounds:campgrounds}); 
-})
-
-router.get("/campgrounds/new", (req,res)=>{
-    res.render("newcampground");
-})
-
-router.post("/campgrounds", (req,res)=>{
-    // add new campground
-    var name  = req.body.name
-    var image = req.body.image
-    var newcampground = {name:name, image:image}
-    campgrounds.push(newcampground);
-    res.redirect('/campgrounds');
-})
 
 module.exports = router
