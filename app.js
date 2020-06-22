548a893a9898f68a4fe944d1c5e7710faed02633
smit-sms <smit16@somaiya.edu>
2020-06-22 18:44:40 +0530

    Refactoring changes

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const mongoose = require('mongoose');
 const url      = 'mongodb://localhost:27017/Adventure-Venture';
 const seedDB   = require('./seeds');
 
-mongoose.connect(url, {useNewUrlParser:true})
-const con      = mongoose.connection
+mongoose.connect(url, {useNewUrlParser:true});
+const con      = mongoose.connection;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
 seedDB();
 con.on('open', () => {
-    console.log("connected...")
+    console.log("connected...");
 })
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 
 
-app.use('/',routes)
+app.use('/',routes);
 
 app.listen(port, ()=>{
-    console.log(`Server started on port ${port}`)
+    console.log(`Server started on port ${port}`);
 })
