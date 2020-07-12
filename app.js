da15852442e4c9b893ee72d3fe438dc5e1766594
smit-sms <smit16@somaiya.edu>
2020-07-13 01:39:09 +0530

    Updated Readme

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express        = require('express');
 const app            = express();
-const port           = process.env.PORT || 5020;
+const port           = process.env.PORT || 3000;
 const mongoose       = require('mongoose');
 const passport       = require('passport');
 const LocalStrategy  = require('passport-local');
